d5709bcc5504b96628159a6f7e2543b003a51ed3
Alina Fedorova <alina-mufler@yandex.ru>
2023-10-22 21:55:15 +0300

    add new ang update files

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,36 @@
 from django.db import models
+from django.urls import path
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    group = models.ForeignKey(
+        Group,
+        related_name='groups',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+
 
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,41 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import logging
+
+from django.shortcuts import render, get_object_or_404
+
+from posts.models import Post, Group
+
+logging.basicConfig(filename='logs.log',
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 # Главная страница
 def index(request):
-    return HttpResponse('Привет, ты попал на главную страницу блога.\n'
-                        'Надеюсь, тебе тут понравится!')
+
+    template = "posts/index.html"
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 # Страница с постами, отфильтрованными по группам
-def group_posts(request, pk):
-    return HttpResponse(f'На данной странице ты можешь прочить посты группы {pk} ')
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
 
